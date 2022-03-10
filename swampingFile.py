c4c867adef430cc060f2c6c4a9ce93c868440d0e
DeepaDidharia <79315183+DeepaDidharia@users.noreply.github.com>
2022-03-10 17:54:59 +0530

    Add files via upload

---
+++ swampingFile.py
@@ -0,0 +1,17 @@
+def swapFileData():
+
+    file1 = input("Enter the original file:")
+    file2 = input("Enter the file to be swapped:")
+
+    with open(file1,'r') as a:
+        data_a = a.read()
+    with open(file2,'r') as b:
+        data_b = b.read()
+
+
+    with open(file1,'w') as a:
+        a.write(data_b)
+    with open(file2,'w') as b:
+        b.write(data_a)
+
+swapFileData()   
